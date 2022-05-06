20ede473efdd9dd3ff43008cb891969b9d523e8c
williamneokh <75356102+williamneokh@users.noreply.github.com>
2022-05-06 16:41:05 +0800

    update files

---
+++ config/config.go
@@ -0,0 +1,5 @@
+package config
+
+//Token is the bot's api token. GroupID is the chat group channel id on telegram
+const Token = "5397961312:AAHYg4c6WOoNbl9ekA9JzFcG7pCg6_vIMqc"
+const GroupID = "-791044219"
